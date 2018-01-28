dd6835b99eebbc24d470232c6db9dba04bb49241
el que m'est <14348482+elquimista@users.noreply.github.com>
2018-01-28 20:56:31 +0700

    Fix routes using Switch

--- src/screens/_Router/Router.js
+++ src/screens/_Router/Router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ConnectedRouter } from 'react-router-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { history } from 'store';
 import { scopes, routes } from 'screens/routes';
 
 export default function Router() {
   return (
     <ConnectedRouter history={history}>
-      <div>
+      <Switch>
         {scopes.map(scope => (
           <scope.module key={scope.module.name}>
             {scope.routes.map(({ path, component }) => (
@@ -18,7 +18,7 @@ export default function Router() {
         {routes.map(({ path, component }) => (
           <Route key={path || ''} exact path={path} component={component} />
         ))}
-      </div>
+      </Switch>
     </ConnectedRouter>
   );
 }
